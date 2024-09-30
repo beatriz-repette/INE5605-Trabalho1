01ff1b9df89a7bb0451948f36e7f10a052208dee
uMilks <94060020+uMilks@users.noreply.github.com>
2024-09-30 09:34:55 -0300

    Quarto commit de Patrick
    Correção de Sistema para compatibilidade com Doacao e Adocao, diagrama atualizado, leve correção de erros em classes feitas pela Beatriz e adição de sintaxe utilizada para testar a eficácia do Sistema.

--- doador.py
+++ doador.py
@@ -3,5 +3,5 @@ from datetime import date
 
 
 class Doador(Pessoa):
-    def __init__(cpf: str, nome: str, data_nascimento: date, endereco: str):
+    def __init__(self, cpf: str, nome: str, data_nascimento: date, endereco: str):
         super().__init__(cpf, nome, data_nascimento, endereco)
