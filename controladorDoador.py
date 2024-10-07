b635de74e3a525f9e0862f1f5e5af2100f1d37d4
Bea <beatriz.repette@gmail.com>
2024-10-07 16:33:05 -0300

    Create controladorDoador.py

---
+++ controladorDoador.py
@@ -0,0 +1,58 @@
+from telaDoador import TelaDoador
+from doador import Doador
+
+
+class ControladorDoador:
+    def __init__(self, controladorPrincipal):
+        self.doadores = []
+        self.__telaDoador = TelaDoador()
+        self.__controladorPrincipal = controladorPrincipal
+
+def doador_por_cpf(self, cpf):
+    for doador in self.__doadores:
+        if doador.cpf == cpf:
+            return doador
+    return None
+
+def incluir_doador(self):
+    dados_doador = self.__telaDoador.pega_dados_doador()
+    for doa in doadores:
+        if doa.cpf == dados_doador["cpf"]:
+            return
+    doador = Doador(dados_doador["cpf"], dados_doador["nome"], dados_doador["data_nascimento"], dados_doador["endereco"])
+    self.__doador.append(doador)
+
+def alterar_doador(self):
+    self.doadores() #Para exibir a lista antes de ser alterada, isso eh opcional
+    cpf_doador = self.__telaDoador.seleciona_doador() #Seleciona retorna o cpf para fazer buscas em listas
+    doador = self.doador_por_cpf(cpf_doador)
+
+    if doador is not None:
+        novos_dados_doador = self.__telaDoador.pega_dados_doador()
+        doador.cpf = novos_dados_doador["cpf"]
+        doador.nome = novos_dados_doador["nome"]
+        doador.data_nascimento = novos_dados_doador["data_nascimento"]
+        doador.endereco = novos_dados_doador["endereco"]
+    #Talvez exibir uma mensagem caso na exista esse amigo? Ou da -pra, na tela, so aceitar cpf validos
+
+def excluir_doador(self):
+    self.doadores() #Para exibir a lista antes de ser alterada, isso eh opcional
+    cpf_doador = self.__telaDoador.seleciona_doador() #Seleciona retorna o cpf para fazer buscas em listas
+    doador = self.doador_por_cpf(cpf_doador)
+
+    if doador is not None:
+        self.__doadores.remove(doador)
+    #Opcional: adicionar mensagem caso doador a ser deletado ja nao exista
+
+@property
+def doadores(self):
+    return self.__doadores
+
+def finalizar(self): #Mudar o nome para 'retornar'?
+    self.__controladorPrincipal.abre_tela()
+
+def abre_tela(self): #anteriormente funcao se chamava "iniciar"
+    lista_opcoes = {}
+
+    while True: #no exemplo ta como 'continua'
+        lista_opcoes[self.__telaDoador.tela_opcoes()]()
