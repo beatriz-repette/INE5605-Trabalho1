5e5d7f4c5f003500004374b3c994cbd4386dd502
uMilks <94060020+uMilks@users.noreply.github.com>
2024-10-22 11:42:46 -0300

    Delete doador.py

--- doador.py
+++
@@ -1,7 +0,0 @@
-from pessoa import Pessoa
-from datetime import date
-
-
-class Doador(Pessoa):
-    def __init__(self, cpf: str, nome: str, data_nascimento: date, endereco: str):
-        super().__init__(cpf, nome, data_nascimento, endereco)
