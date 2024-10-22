d3faa8a38bbb3ef85c7d26f02610dfd68455d791
Bea <beatriz.repette@gmail.com>
2024-10-21 21:41:33 -0300

    Update controladorPrincipal.py

--- controle/controladorPrincipal.py
+++ controle/controladorPrincipal.py
@@ -1,10 +1,12 @@
 from controle.controladorDoador import ControladorDoador
 from limite.telaPrincipal import TelaPrincipal
+from controle.controladorAdotante import  ControladorAdotante
 
 
 class ControladorPrincipal:
     def __init__(self):
         self.__controladorDoador = ControladorDoador(self)
+        self.__controladorAdotante = ControladorAdotante(self)
         self.__tela_principal = TelaPrincipal()
 
     @property
@@ -15,6 +17,10 @@ class ControladorPrincipal:
         #Chama o controlador de doador
         self.__controladorDoador.abre_tela()
 
+    def cadastra_adotante(self):
+        #Chama o controlador de adotante
+        self.__controladorAdotante.abre_tela()
+
     def iniciar_controlador(self):
         self.abre_tela()
 
@@ -23,7 +29,7 @@ class ControladorPrincipal:
 
     def abre_tela(self):
         #Ir adicionando mais opcoes a media que os controladores/telas forem sendo adicionados
-        lista_opcoes = {1: self.cadastra_doador, 0: self.encerra_sistema}
+        lista_opcoes = {1: self.cadastra_doador, 2: self.cadastra_adotante, 0: self.encerra_sistema}
 
         while True:
             opcao_escolhida = self.__tela_principal.tela_opcoes()
