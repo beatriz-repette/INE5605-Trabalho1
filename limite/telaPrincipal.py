77926b05eab7ff73c7784dc7d190a4c39cbff084
Bea <beatriz.repette@gmail.com>
2024-10-21 21:42:33 -0300

    Update telaPrincipal.py

--- limite/telaPrincipal.py
+++ limite/telaPrincipal.py
@@ -5,11 +5,11 @@ class TelaPrincipal():
         print("-------- ONG de Animais ---------")
         print("Escolha sua opcao")
         print("1 - Doador")
+        print("2 - Adotante")
         print("0 - Finalizar sistema")
 
         opcao = input("Escolha a opcao: ")
-        while opcao not in ["1","0"]: #Adicionar opcoes a medida que novas telas forem sendo adicionadas
+        while opcao not in ["1","2","0"]: #Adicionar opcoes a medida que novas telas forem sendo adicionadas
             print("Input invalido, por favor digite uma das opcoes validas")
             opcao = input("Escolha a opcao: ")
-
         return int(opcao)
