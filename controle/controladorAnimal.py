6ac66c33eeca6a4a6e6665db995fe0e6eeae4559
uMilks <94060020+uMilks@users.noreply.github.com>
2024-11-25 21:48:22 -0300

    Add files via upload

--- controle/controladorAnimal.py
+++ controle/controladorAnimal.py
@@ -13,9 +13,10 @@ class ControladorAnimal():
         while True:
             switch = {
                 0: self.finalizar,
-                1: self.__controladorPrincipal.visualizar_cachorro,
-                2: self.__controladorPrincipal.visualizar_gato
+                1: self.__controladorPrincipal.visualizar_gato,
+                2: self.__controladorPrincipal.visualizar_cachorro
             }
             opcao = self.__telaAnimal.tela_opcoes()
             funcao_escolhida = switch[opcao]
             funcao_escolhida()
+            break
