363f92721e9f3473ecab328c233f0d2a33bb2c3e
uMilks <94060020+uMilks@users.noreply.github.com>
2024-12-13 09:56:00 -0300

    DAOs e Gráficos
    Mudanças para adicionar DAOs e interfaces gráficas.

--- controle/controladorGato.py
+++ controle/controladorGato.py
@@ -1,5 +1,6 @@
 from entidade.gato import Gato
 from limite.telaGato import TelaGato
+from daos.daoGato import GatoDAO
 from entidade.vacinacao import Vacinacao, Vacina
 from exception.erroVacinaException import ErroVacinaException
 
@@ -7,24 +8,24 @@ class ControladorGato():
     def __init__(self, cont_principal) -> None:
         self.__controladorPrincipal = cont_principal
         self.__telaGato = TelaGato()
-        self.__gatos = []
+        self.__gato_DAO = GatoDAO()
 
     @property
     def gatos(self):
-        return self.__gatos
+        return self.__gato_DAO.get_all()
 
     def gato_por_chip(self, id):
-        for gato in self.__gatos:
+        for gato in self.gatos:
             if gato.num_chip == id:
                 return gato
         return 'Gato nao se encontra no sistema.'
 
     def listar_gatos(self):
-        if self.__gatos == []:
+        if self.gatos == []:
             self.__telaGato.mensagem_sem_gatos()
         else:
             dados = []
-            for a in self.__gatos:
+            for a in self.gatos:
                 nomes_vacinas = ''
                 n = 0
                 for vac in a.vacinas:
@@ -44,45 +45,54 @@ class ControladorGato():
             self.__telaGato.mostrar_gato(dados)
 
     def incluir_gato(self, dados):
+        # ADICIONAR ESSE FOR EM CACHORRO
+        for gato in self.gatos:
+            if gato.num_chip == dados['chip']:
+                return None
         cat = Gato(dados['chip'], dados['nome'], dados['raca'], dados['vacinas'])
-        self.__gatos.append(cat)
+        self.__gato_DAO.add(cat)
         return cat
     
     def adicionar_vacina(self):
-        try:
-            dados = self.__telaGato.pegar_vacina()
-            gato = self.gato_por_chip(dados[0]['animal'])
-            if gato != 'Gato nao se encontra no sistema.':
-                for v in dados:
-                    gato.add_vacina(self.__controladorPrincipal.controladorVacinacao.incluir_vacinacao(v['data'], v['nome'], v['animal']))
-            else:
-                raise ErroVacinaException
-        except:
-            self.__telaGato.mensagem_erro_vacina()
+        if self.gatos == []:
+            self.__telaGato.mensagem_sem_gatos()
+        else:
+            try:
+                chip = self.__telaGato.seleciona_gato()
+                gato = self.gato_por_chip(chip)
+                if gato != 'Gato nao se encontra no sistema.':
+                    dados = self.__telaGato.pegar_vacina(chip)
+                    for v in dados:
+                        gato.add_vacina(self.__controladorPrincipal.controladorVacinacao.incluir_vacinacao(v['data'], v['nome'], v['animal']))
+                    self.__gato_DAO.update(gato, gato.num_chip)
+                else:
+                    raise ErroVacinaException
+            except:
+                self.__telaGato.mensagem_erro_vacina()
     
     def alterar_gato(self):
-        if self.__gatos == []:
+        if self.gatos == []:
             self.__telaGato.mensagem_sem_gatos()
         else:
             chip = self.__telaGato.seleciona_gato()
             gato = self.gato_por_chip(chip)
             if gato != 'Gato nao se encontra no sistema.':
                 dados = self.__telaGato.pegar_dados_gato()
-                # Se for *, não substitui o valor antigo
+                
+                # Se dados['dado'] for *, não substitui o valor antigo
                 if dados['chip'] != '*':
+                    # Se o chip não existir ou for o próprio
                     if (self.__controladorPrincipal.animal_por_chip(dados['chip']) == 'Animal não se encontra no sistema.'
                         or dados['chip'] == gato.num_chip):
                         doacao = self.__controladorPrincipal.controladorDoacao
                         for doa in doacao.doacoes:
                             if doa.animal == gato.num_chip:
+                                # ISSO VAI MUDAR QUANDO MUDAR DOACAO
                                 doa.animal = dados['chip']
                         gato.num_chip = dados['chip']
 
                     else:
                         self.__telaGato.mostrar_mensagem('Ja existe um animal com esse chip.')
-                else:
-                    # Coloca o chip como o atual para usar na vacinacao
-                    dados['chip'] = gato.num_chip
 
                 if dados['nome'] != '*':
                     gato.nome = dados['nome']
@@ -95,25 +105,28 @@ class ControladorGato():
                         if vac.animal_chip == chip:
                             self.__controladorPrincipal.controladorVacinacao.excluir_vacinacao(vac)
                     for vac in gato.vacinas:
-                        gato.excluir_vacina(vac)
+                        if vac != '':
+                            gato.excluir_vacina(vac)
 
                     for v in dados['vacinas']:
-                        vacina = self.__controladorPrincipal.controladorVacinacao.incluir_vacinacao(v['data'], v['nome'], dados['chip'])
-                        gato.add_vacina(vacina)
+                        if v['data'] != '':
+                            vacina = self.__controladorPrincipal.controladorVacinacao.incluir_vacinacao(v['data'], v['nome'], gato.num_chip)
+                            gato.add_vacina(vacina)
 
+                self.__gato_DAO.update(gato, chip)
                 self.__telaGato.mensagem_operacao_concluida()
 
             else:
                 self.__telaGato.mostrar_mensagem(gato)
 
     def excluir_gato(self):
-        if self.__gatos == []:
+        if self.gatos == []:
             self.__telaGato.mensagem_sem_gatos()
         else:
             chip = self.__telaGato.seleciona_gato()
             gato = self.gato_por_chip(chip)
             if gato != 'Gato nao se encontra no sistema.':
-                self.__gatos.remove(gato)
+                self.__gato_DAO.remove(gato.num_chip)
             else:
                 self.__telaGato.mostrar_mensagem(gato)
 
