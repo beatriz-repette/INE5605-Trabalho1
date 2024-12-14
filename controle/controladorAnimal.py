7da9647f7d6d2a1bae431d18b95e7587338a861b
uMilks <94060020+uMilks@users.noreply.github.com>
2024-12-13 21:47:09 -0300

    Mudando animal_por_chip
    Mudando animal_por_chip de controladorPrincipal para controladorAnimal (desnecessariamente kk)

--- controle/controladorAnimal.py
+++ controle/controladorAnimal.py
@@ -3,11 +3,26 @@ class ControladorAnimal():
         from controle.controladorPrincipal import ControladorPrincipal
         self.__controladorPrincipal = contPrincipal
 
+        from controle.controladorCachorro import ControladorCachorro
+
+        self.__controlador_cachorro = self.__controladorPrincipal.controladorCachorro
+        from controle.controladorGato import ControladorGato
+        self.__controlador_gato = self.__controladorPrincipal.controladorGato
+
         from limite.telaAnimal import TelaAnimal
         self.__telaAnimal = TelaAnimal()
 
     def finalizar(self):
         self.__controladorPrincipal.abre_tela()
+
+    def animal_por_chip(self, id):
+        cachorro = self.__controlador_cachorro.cachorro_por_chip(id)
+        if cachorro != 'Cachorro nao se encontra no sistema.':
+            return cachorro
+        gato = self.__controlador_gato.gato_por_chip(id)
+        if gato != 'Gato nao se encontra no sistema.':
+            return gato
+        return 'Animal não se encontra no sistema.'
 
     def abrir_tela(self):
         while True:
